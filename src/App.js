3447ead32a3d6011134e28a3ff1912b0b8e03683
UADACID <pratamasetya99@gmail.com>
2020-01-03 18:59:03 +0700

    init minimum configuration

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <>
+        <h1>Halo React dan Webpack</h1>
+        <p>Minimal konfigurasi</p>
+    </>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
